076c0042c5366115759197581f4cf2d7aa48f619
IndrajitSir <indrajitmandal779@gmail.com>
2024-01-16 09:06:52 +0530

    Api and error handling, database connection DONE

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import { error } from "console";
+
+import { app } from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()// jab bhi ek asynchronous method complete hota hai to wo ek promise return krta hai.
+.then(()=>{
+    app.on("error", ()=>{ //RAKH SAKTA HOON,,,,....RAKHNA CHAHIYE BASICALLY
+        console.error("ERROR:",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is listening on ${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log("MongoDB connection Failed", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", ()=>{
+            console.error("ERROR:",error);
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR:",error);
+        throw error
+    }
+})()
+*/
