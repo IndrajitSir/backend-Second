f8d2c9e78d673bd33c8ee3a826712071038946c3
IndrajitSir <indrajitmandal779@gmail.com>
2024-01-16 09:46:36 +0530

    User Model and Video Model created

---
+++ src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        coverImage: {
+            type: String
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is Required"] // true field ke sath ek custom message bhi de sakte hain
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    { 
+        timestamps: true
+    }
+);
+
+
+export const User = mongoose.model("User", userSchema);
